f8dd37bc1d26f7428ca9c10d2321fe138b61a7b4
prangya <prangyaparamitaray20@gmail.com>
2026-02-24 10:36:54 +0530

    basic program

---
+++ niis python/basic program/cyli.py
@@ -0,0 +1,8 @@
+print("enter radius")
+r=float(input())
+print("enter height")
+h=float(input())
+A=2*3.14*r*(r+h)
+print("total surface area=",A)
+
+
---
+++ niis python/basic program/reverse.py
@@ -0,0 +1,4 @@
+print("enter 2 digit number")
+n=int(input())
+rev=(n%10)*10+(n//10)
+print("reverse=",rev)
